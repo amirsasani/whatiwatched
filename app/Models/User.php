981523deceaa8f06f37b5770de2b18cbd154bfc6
Amir Sasani <sasani.amir.1998@gmail.com>
2022-05-10 22:22:11 +0430

    make relation between titles and users

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function titles()
+    {
+        return $this->belongsToMany(Title::class, 'title_user');
+    }
 }
