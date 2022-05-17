f77825bef3c08ca3430cf48e3aa142d0af897977
Amir Sasani <sasani.amir.1998@gmail.com>
2022-05-17 22:36:20 +0430

    add slug to titles

--- app/Models/Title.php
+++ app/Models/Title.php
@@ -2,14 +2,16 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Title extends Model
 {
     use HasFactory;
+    use Sluggable;
 
-    protected $fillable = ["imdb_url", "name", "rate_value", "rate_count", "year_start", "year_end", "genres", "poster"];
+    protected $fillable = ["imdb_url", "slug", "name", "rate_value", "rate_count", "year_start", "year_end", "genres", "poster"];
 
     protected $casts = [
         "genres" => "array"
@@ -25,4 +27,20 @@ class Title extends Model
     {
         return $this->belongsToMany(User::class, 'title_user');
     }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+
 }
