00277ec7df09e70199efbbb7f9587c7f4cca6e48
Amir Sasani <sasani.amir.1998@gmail.com>
2022-06-06 15:04:08 +0430

    fix route function in services config file

--- config/services.php
+++ config/services.php
@@ -34,13 +34,13 @@ return [
     'github' => [
         'client_id' => env('GITHUB_CLIENT_ID'),
         'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => route('auth.callback', ['provider' => 'github']),
+        'redirect' => env('GITHUB_REDIRECT'),
     ],
 
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => route('auth.callback', ['provider' => 'google']),
+        'redirect' => 'google/callback',
     ],
 
 ];
