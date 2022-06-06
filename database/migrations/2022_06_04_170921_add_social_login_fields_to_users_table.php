72598ebdbc86d514504ac6ba6d23845ef3a09a67
Amir Sasani <sasani.amir.1998@gmail.com>
2022-06-06 15:41:24 +0430

    add after in social login fields migration

--- database/migrations/2022_06_04_170921_add_social_login_fields_to_users_table.php
+++ database/migrations/2022_06_04_170921_add_social_login_fields_to_users_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('provider_name')->nullable();
-            $table->string('provider_id')->nullable();
+            $table->string('provider_name')->after('email')->nullable();
+            $table->string('provider_id')->after('provider_name')->nullable();
         });
     }
 
